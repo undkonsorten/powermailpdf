066923c9b6b1057bd428f84f0a37661bb3b17920
eike.starkmann <eike.starkmann@undkonsorten.com>
2018-06-06 13:44:58 +0200

    [ADD]: new version

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array (
 	'uploadfolder' => true,
 	'createDirs' => '',
 	'clearCacheOnLoad' => 0,
-	'version' => '2.4.3',
+	'version' => '2.4.4',
     'autoload' =>
         array(
             'psr-4' => array('Undkonsorten\\Powermailpdf\\' => 'Classes')
