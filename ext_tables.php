5b9a5ee526d05b485285cb79b9f265507786fe6a
Sybille Peters <sypets@gmx.de>
2018-08-31 15:30:41 +0200

    [FIX] Fix strings in ext_tables.php
    Resolves: #4

--- ext_tables.php
+++ ext_tables.php
@@ -20,10 +20,9 @@ if (!defined('TYPO3_MODE')) {
 
 
 
-$pluginSignature = str_replace('_','',$_EXTKEY) . '_' . pdf;
+$pluginSignature = str_replace('_','',$_EXTKEY) . '_' . 'pdf';
 $TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_' .pdf. '.xml');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_pdf.xml');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Powermail PDF Form');
 
