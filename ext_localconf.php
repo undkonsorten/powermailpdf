b7884f152c3f498d662445021e2ceb972867976b
Sybille Peters <sypets@gmx.de>
2018-08-31 16:35:54 +0200

    [CLEANUP] Remove unused code
    Conflicts:
    	ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,24 +3,9 @@ if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'Undkonsorten.'.$_EXTKEY,
-	'Pdf',
-	array(
-		'PDF' => 'main',
-		
-	),
-	// non-cacheable actions
-	array(
-		'PDF' => 'main',
-		
-	)
-);
 \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\SignalSlot\\Dispatcher')->connect(
 		'In2code\\Powermail\\Controller\\FormController',
 		'createActionBeforeRenderView',
 		\Undkonsorten\Powermailpdf\Pdf::class,
 		'createAction'
 );
-
-?>
