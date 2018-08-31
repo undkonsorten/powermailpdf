6c91a460e72c9316a3d4ed03c37cb2aea6686ec3
Sybille Peters <sypets@gmx.de>
2018-08-31 16:35:54 +0200

    [CLEANUP] Remove unused code

--- ext_tables.php
+++ ext_tables.php
@@ -5,27 +5,5 @@ if (!defined('TYPO3_MODE')) {
 
 
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-		$_EXTKEY,
-		'Pi1',
-		array(
-				#...
-				'Pdf' => 'main',
-				#...
-		),
-		array(
-				'Pdf' => 'main'
-		)
-);
-
-
-
-$pluginSignature = str_replace('_','',$_EXTKEY) . '_' . 'pdf';
-$TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_pdf.xml');
-
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Powermail PDF Form');
 
-
-
-?>
