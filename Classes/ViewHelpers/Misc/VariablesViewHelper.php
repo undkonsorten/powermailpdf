32c1f40272a138fbffa8676719dfea0a5aa2f28c
Hackl Christian <hackl.chris@googlemail.com>
2019-10-15 18:09:31 +0200

    fix viewhelper arguments for typo3 9 - fixed downloadLink for configs e. g. without baseURL set

--- Classes/ViewHelpers/Misc/VariablesViewHelper.php
+++ Classes/ViewHelpers/Misc/VariablesViewHelper.php
@@ -1,6 +1,7 @@
 <?php
 namespace Undkonsorten\Powermailpdf\ViewHelpers\Misc;
 
+// use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
 use In2code\Powermail\Domain\Model\Mail;
 use In2code\Powermail\Utility\ArrayUtility;
 use In2code\Powermail\Utility\TemplateUtility;
@@ -54,25 +55,46 @@ class VariablesViewHelper extends AbstractViewHelper
     protected $settings = [];
 
     /**
+     * @return void
+     */
+    public function initializeArguments()
+    {
+        parent::initializeArguments();
+        $this->registerArgument('mail', Mail::class, 'Mail', true);
+        $this->registerArgument('type', 'string', '"web" or "mail"', false, 'web');
+        $this->registerArgument('function', 'string', 'createAction, senderMail, receiverMail', false, 'createAction');
+    }
+
+    /**
      * Enable variables within variable {powermail_rte} - so string will be parsed again
      *
-     * @param Mail $mail Variables and Labels array
-     * @param string $type "web" or "mail"
-     * @param string $function "createAction", "senderMail", "receiverMail"
-     * @return string Changed string
+     * @return string
      */
-    public function render(Mail $mail, $type = 'web', $function = 'createAction')
+    public function render(): string
     {
+        $mail = $this->arguments['mail'];
+        $type = $this->arguments['type'];
+        $function = $this->arguments['function'];
         /** @var StandaloneView $parseObject */
         $parseObject = $this->objectManager->get(StandaloneView::class);
         $parseObject->setTemplateSource($this->removePowermailAllParagraphTagWrap($this->renderChildren()));
 
         $variables = $this->mailRepository->getVariablesWithMarkersFromMail($mail);
-        foreach ($variables as $key => $value){
-            if($key != 'downloadLink'){
+
+        foreach ($variables as $key => $value) {
+            if($key != 'downloadLink') {
                 $variables[$key] = html_entity_decode($value);
+            } else {
+                // I don't know if this is a problem for (e. g.) TYPO3 with baseURL set - so:
+                // If link starts with slash change this to a full abs link/url
+                $query = '/';
+                $link = explode('<a href="', $value)[1];
+                if(substr($link, 0, strlen($query)) === $query) {
+                    $variables['downloadLink'] = '<a href="'. GeneralUtility::getIndpEnv('TYPO3_REQUEST_HOST') . explode('<a href="', $value)[1];
+                }
             }
         }
+
         $parseObject->assignMultiple(
             $variables
         );
@@ -80,6 +102,7 @@ class VariablesViewHelper extends AbstractViewHelper
             ArrayUtility::htmlspecialcharsOnArray($this->mailRepository->getLabelsWithMarkersFromMail($mail))
         );
         $parseObject->assign('powermail_all', TemplateUtility::powermailAll($mail, $type, $this->settings, $function));
+
         return html_entity_decode($parseObject->render(), ENT_QUOTES, 'UTF-8');
     }
 
