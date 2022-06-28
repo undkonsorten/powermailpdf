3d7a45dbe26f502b91417d535944de2989671f09
Sybille Peters <sypets@gmx.de>
2022-06-28 18:57:06 +0200

    Change version constraints for v11

--- ext_emconf.php
+++ ext_emconf.php
@@ -30,9 +30,9 @@ $EM_CONF[$_EXTKEY] = array (
 	array (
 		'depends' =>
 		array (
-            'typo3' => '10.4.0-10.99.99',
-            'php' => '7.2.0-7.99.99',
-			'powermail' => '8.0.0-8.99.99',
+            'typo3' => '10.4.0-11.99.99',
+            'php' => '7.2.0-8.99.99',
+			'powermail' => '8.0.0-19.99.99',
 		),
 		'conflicts' =>
 		array (
