be0303c0e929e4567d776f521c8f7d693016dd62
ole <ole.mollenhauer@vancado.de>
2023-11-30 11:52:28 +0100

    [TASK] Update ViewHelper

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,16 +3,8 @@
 use Undkonsorten\Powermailpdf\ViewHelpers\Misc\VariablesViewHelper;
 
 if (!defined('TYPO3')) {
-	die ('Access denied.');
+    die ('Access denied.');
 }
-/*
-\TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class)->connect(
-		\In2code\Powermail\Controller\FormController::class,
-		'createActionBeforeRenderView',
-		\Undkonsorten\Powermailpdf\Pdf::class,
-		'createActionBeforeRenderView'
-);
-*/
 
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\In2code\Powermail\ViewHelpers\Misc\VariablesViewHelper::class] = [
     'className' => VariablesViewHelper::class
