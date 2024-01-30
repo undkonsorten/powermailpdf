2dbe84a0e07b26d4cecff7ca3c4007f76eaa86fe
Konstantin Berendakov <konstantin.berendakov@vancado.de>
2024-01-30 11:06:21 +0100

    [TASK] Add pdf flattening and encoding

--- Classes/EventListener/CreateActionBeforeRenderView.php
+++ Classes/EventListener/CreateActionBeforeRenderView.php
@@ -14,8 +14,12 @@ use TYPO3\CMS\Core\Resource\ResourceFactory;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Utility\LocalizationUtility;
+use TYPO3\CMS\Core\TypoScript\Parser\TypoScriptParser;
+use TYPO3\CMS\Core\TypoScript\TypoScriptService;
+use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
 use TYPO3\CMS\Fluid\View\StandaloneView;
 use In2code\Powermail\Events\FormControllerCreateActionBeforeRenderViewEvent;
+
 
 /**
  * PDF handling.
@@ -26,6 +30,8 @@ final class CreateActionBeforeRenderView
     /** @var ResourceFactory */
     protected $resourceFactory;
     private StandaloneView $standaloneView;
+
+    protected ?bool $encoding = null;
 
     public function __construct(ResourceFactory $resourceFactory, StandaloneView $standaloneView)
     {
@@ -42,6 +48,7 @@ final class CreateActionBeforeRenderView
     {
 
         $settings = $GLOBALS['TSFE']->tmpl->setup['plugin.']['tx_powermailpdf.']['settings.'];
+        $this->encoding = $settings['encoding'];
 
         /** @var Folder $folder */
         $folder = $this->resourceFactory->getFolderObjectFromCombinedIdentifier($settings['target.']['pdf']);
@@ -54,13 +61,53 @@ final class CreateActionBeforeRenderView
         //Normal Fields
         $fieldMap = $GLOBALS['TSFE']->tmpl->setup['plugin.']['tx_powermailpdf.']['settings.']['fieldMap.'];
 
-        $answers = $mail->getAnswers();
-        $fdfDataStrings = array();
+        //Normal Fields
+        $fieldMap = $settings['fieldMap.'];
 
-        foreach ($fieldMap as $key => $value) {
+        $answers = $mail->getAnswers();
+
+        $fdfDataStrings = array();
+        $pdfField_value = null;
+        foreach ($fieldMap as $fieldID => $fieldConfig) {
             foreach ($answers as $answer) {
-                if ($value == $answer->getField()->getMarker()) {
-                    $fdfDataStrings[$key] = $answer->getValue();
+                $pdfField_name = explode('.', $fieldID)[0];
+
+                if (is_array($fieldConfig)) {
+                    $pdfField_type = $fieldConfig['type'];
+                    $pdfField_value = $fieldConfig['form_value'];
+                    $formField_name = $fieldConfig['form_name'];
+                } else {
+                    $pdfField_type = 'text';
+                    $formField_name = $fieldConfig;
+                }
+
+                if ($formField_name == $answer->getField()->getMarker()) {
+                    if ($pdfField_type == 'text') {
+                        $pdfField_value = $this->encodeValue($answer->getValue());
+                    } elseif ($pdfField_type == 'checkbox') {
+                        if ($answer->getValue() == $fieldConfig['form_value']) {
+                            $pdfField_value = $this->encodeValue($fieldConfig['pdf_value']);
+                        }
+                    }
+                } else {
+                    continue;
+                }
+                $fdfDataStrings[$pdfField_name] = $pdfField_value;
+            }
+        }
+
+        // Variables
+        if (isset($settings['variables.'])) {
+            $variables = $settings['variables.'];
+
+            if (!empty($variables)) {
+                $typoScriptService = GeneralUtility::makeInstance(TypoScriptService::class);
+                $variables = $typoScriptService->convertTypoScriptArrayToPlainArray($variables);
+                $cObject = GeneralUtility::makeInstance(ContentObjectRenderer::class);
+                foreach ($variables as $key => $item) {
+                    $type = $item['_typoScriptNodeValue'];
+                    unset($item['_typoScriptNodeValue']);
+                    $fdfDataStrings[$key] = $cObject->cObjGetSingle($type, $item);
                 }
             }
         }
@@ -68,16 +115,43 @@ final class CreateActionBeforeRenderView
         $pdfOriginal = GeneralUtility::getFileAbsFileName($GLOBALS['TSFE']->tmpl->setup['plugin.']['tx_powermailpdf.']['settings.']['sourceFile']);
 
         if (!empty($pdfOriginal)) {
+            $pdfFlatTempFile = (string) null;
             $info = pathinfo($pdfOriginal);
             $pdfFilename = basename($pdfOriginal, '.' . $info['extension']) . '_';
             $pdfTempFile = GeneralUtility::tempnam($pdfFilename, '.pdf');
-            $pdf = new FPDM($pdfOriginal);
-            $pdf->Load($fdfDataStrings, true); // second parameter: false if field values are in ISO-8859-1, true if UTF-8
+
+            $pdf = new \FPDM($pdfOriginal);
+            $pdf->Load($fdfDataStrings, !$this->encoding); // second parameter: false if field values are in ISO-8859-1, true if UTF-8
             $pdf->Merge();
             $pdf->Output("F", GeneralUtility::getFileAbsFileName($pdfTempFile));
+
+            if ($settings['flatten'] && $settings['flattenTool']) {
+                $pdfFlatTempFile = GeneralUtility::tempnam($pdfFilename, '.pdf');
+                $tempFile = GeneralUtility::tempnam($pdfFilename, '.pdf');
+                switch ($settings['flattenTool']) {
+                    case 'gs':
+                        // Flatten PDF with ghostscript
+                        @shell_exec("gs -sDEVICE=pdfwrite -dSubsetFonts=false -dPDFSETTINGS=/default -dNOPAUSE -dBATCH -sOutputFile=" . $pdfFlatTempFile . " " . $pdfTempFile);
+                        break;
+                    case 'pdftocairo':
+                        // Flatten PDF with pdftocairo
+                        @shell_exec('pdftocairo -pdf ' . $pdfTempFile . ' ' . $pdfFlatTempFile);
+                        break;
+                    case 'pdftk':
+                        // Flatten PDF with pdftk
+                        @shell_exec('pdftk ' . $pdfTempFile . ' generate_fdf output ' . $tempFile);
+                        @shell_exec('pdftk ' . $pdfTempFile . ' fill_form ' . $tempFile . ' output ' . $pdfFlatTempFile . ' flatten');
+                        break;
+                }
+            }
         } else {
             throw new Exception("No pdf file is set in Typoscript. Please set tx_powermailpdf.settings.sourceFile if you want to use the filling feature.", 1417432239);
         }
+
+        if (file_exists($pdfFlatTempFile)) {
+            return $folder->addFile($pdfFlatTempFile);
+        }
+
         return $folder->addFile($pdfTempFile);
     }
 
@@ -88,7 +162,6 @@ final class CreateActionBeforeRenderView
      */
     protected function render(File $file, $label)
     {
-
         $settings = $GLOBALS['TSFE']->tmpl->setup['plugin.']['tx_powermailpdf.']['settings.'];
         $templatePath = GeneralUtility::getFileAbsFileName($settings['template']);
         $this->standaloneView->setFormat('html');
@@ -148,6 +221,21 @@ final class CreateActionBeforeRenderView
                 $settings['sender']['addAttachment']['value'] = $powermailPdfFile->getForLocalProcessing(false);
                 $formController->setSettings($settings);
             }
+        }
+    }
+
+    protected function encodeValue($value)
+    {
+        if ($value == '') {
+            $value = 'k.A.';
+        }
+        if (is_array($value)) {
+            $value = implode(',', $value);
+        }
+        if ($this->encoding) {
+            return iconv('UTF-8', $this->encoding, $value);
+        } else {
+            return $value;
         }
     }
 }
